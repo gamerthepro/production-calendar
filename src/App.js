6f4cf0f5c4f476642050cd2f3e206783ef1c651a
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-09-14 16:02:22 +0300

    add css html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="calendar__container">
+			<div className="calendar__header">
+				<input className="calendar__search"></input>
+				<h1 className="calendar__title">september</h1>
+			</div>
+			<div className="calendar__main">
+				<ul className="calendar__users">User
+					<li className="user__name">Roma</li>
+					<li className="user__name">Vika</li>
+					<li className="user__name">Pasha</li>
+					<li className="user__name">Masha</li>
+				</ul>
+				<div className="calendar__data">
+					<ul className="calendar__days">
+						<li className="day__item">1</li>
+						<li className="day__item">2</li>
+						<li className="day__item">3</li>
+						<li className="day__item">4</li>
+					</ul>
+					<ul className="calendar__time">
+						<li className="time__item">3:22</li>
+						<li className="time__item">3:22</li>
+						<li className="time__item">3:22</li>
+						<li className="time__item">3:22</li>
+					</ul>
+					<ul className="calendar__time">
+						<li className="time__item">3:22</li>
+						<li className="time__item">3:22</li>
+						<li className="time__item">3:22</li>
+						<li className="time__item">3:22</li>
+					</ul>
+					<ul className="calendar__time">
+						<li className="time__item">3:22</li>
+						<li className="time__item">3:22</li>
+						<li className="time__item">3:22</li>
+						<li className="time__item">3:22</li>
+					</ul>
+					<ul className="calendar__time">
+						<li className="time__item">3:22</li>
+						<li className="time__item">3:22</li>
+						<li className="time__item">3:22</li>
+						<li className="time__item">3:22</li>
+					</ul>
+				</div>
+				<ul className="calendar__results">Monthly total
+					<li className="result__item"></li>
+					<li className="result__item"></li>
+					<li className="result__item"></li>
+					<li className="result__item"></li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
